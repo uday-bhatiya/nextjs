9086f2def5c2c6814e6431844719c3f479914cba
Uday Bhatiya <udaybhatiya31@gmail.com>
2024-07-03 16:49:36 +0530

    add upcoming webinars section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import HeroSection from "./components/HeroSection";
 import FeaturedSection from "./components/FeaturedSection";
 import WhyChooseUsSection from "./components/WhyChooseUsSection";
 import TestimonialSection from "./components/TestimonialSection";
+import UpcomingWebinars from "./components/UpcomingWebinarsSection";
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
       <FeaturedSection/>
       <WhyChooseUsSection/>
       <TestimonialSection />
+      <UpcomingWebinars />
     </main>
   );
 }
