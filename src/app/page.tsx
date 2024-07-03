d17ac4f72b0ab736114b14cdc4208ccba5df4df7
Uday Bhatiya <udaybhatiya31@gmail.com>
2024-07-03 13:48:58 +0530

    add featured section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import HeroSection from "./components/HeroSection";
+import FeaturedSection from "./components/FeaturedSection";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.95] antialiased bg-grid-white/[0.02]">
       <HeroSection/>
+      <FeaturedSection/>
     </main>
   );
 }
