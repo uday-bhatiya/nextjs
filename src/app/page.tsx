8c00cd45b0ffa96a81eb9d63a9fb70780fda9e53
Uday Bhatiya <udaybhatiya31@gmail.com>
2024-07-03 17:05:26 +0530

    add instructors section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import FeaturedSection from "./components/FeaturedSection";
 import WhyChooseUsSection from "./components/WhyChooseUsSection";
 import TestimonialSection from "./components/TestimonialSection";
 import UpcomingWebinars from "./components/UpcomingWebinarsSection";
+import InstructorSection from "./components/InstructorSection";
 
 export default function Home() {
   return (
@@ -13,6 +14,7 @@ export default function Home() {
       <WhyChooseUsSection/>
       <TestimonialSection />
       <UpcomingWebinars />
+      <InstructorSection />
     </main>
   );
 }
