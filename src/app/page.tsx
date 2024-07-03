4a6eb9626ac6e7f425561809a370c573f22028ec
Uday Bhatiya <udaybhatiya31@gmail.com>
2024-07-03 16:31:37 +0530

    add why choose us and testimonial section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,16 @@
 import Image from "next/image";
 import HeroSection from "./components/HeroSection";
 import FeaturedSection from "./components/FeaturedSection";
+import WhyChooseUsSection from "./components/WhyChooseUsSection";
+import TestimonialSection from "./components/TestimonialSection";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.95] antialiased bg-grid-white/[0.02]">
       <HeroSection/>
       <FeaturedSection/>
+      <WhyChooseUsSection/>
+      <TestimonialSection />
     </main>
   );
 }
